32325b0a1efd4832ddc5c4954d4f58d9846ebd5a
Beksultan Tuleev <beksultan.tuleev@studenti.unitn.it>
2021-05-21 17:11:24 +0200

    std filter

--- src/RealTIme_Signal_filter.py
+++ src/RealTIme_Signal_filter.py
@@ -9,9 +9,9 @@ import numpy as np
 
 
 class Signal_filter():
-    def __init__(self, sample_size=15, sigma=5):
-        self.sample_size = sample_size
-        self.sigma = sigma
+    def __init__(self):
+        self.sample_size = None
+        self.sigma = None
         self.deque_x = collections.deque([])
         self.deque_y = collections.deque([])
         self.deque_z = collections.deque([])
@@ -23,16 +23,28 @@ class Signal_filter():
         c = Counter(sample)
         return [k for k, v in c.items() if v == c.most_common(1)[0][1]]
 
-    def lauch_gauss_filter(self):
+    def plus_minus(self):
+        value = np.random.random(1)[0]
+        if value > 0.5:
+            return 1
+        return -1
+
+    def mqtt_callback(self):
+        raw_pos = self.mqttSubscriber.pos
+        if raw_pos == []:
+            time.sleep(0.5)
+        time.sleep(0.08)
+        return raw_pos
+
+    def lauch_gauss_filter(self, sample_size, sigma):
+        self.sample_size = sample_size
+        self.sigma = sigma
         while len(self.deque_x) < self.sample_size:  # 15 was nice
-            a = self.mqttSubscriber.pos
-            if a == []:
-                time.sleep(0.5)
-            time.sleep(0.08)  # 0.08 is fine
-            if a:
-                self.deque_x.appendleft(a[0])  # works
-                self.deque_y.appendleft(a[1])
-                self.deque_z.appendleft(a[2])
+            raw_pos = self.mqtt_callback()
+            if raw_pos:
+                self.deque_x.appendleft(raw_pos[0])  # works
+                self.deque_y.appendleft(raw_pos[1])
+                self.deque_z.appendleft(raw_pos[2])
 
                 x_gaus = gaussian_filter(self.deque_x, self.sigma)
                 y_gaus = gaussian_filter(self.deque_y, self.sigma)
@@ -50,16 +62,14 @@ class Signal_filter():
                         deque_x_gaus.pop(), deque_y_gaus.pop(), deque_z_gaus.pop()]
                     return self.final_pos
 
-    def lauch_mode_filter(self):
-        while len(self.deque_x) < self.sample_size:  # 15 was nice
-            a = self.mqttSubscriber.pos
-            if a == []:
-                time.sleep(0.5)
-            time.sleep(0.08)  # 0.08 is fine
-            if a:
-                self.deque_x.appendleft(np.round(a[0], 2))  # works
-                self.deque_y.appendleft(np.round(a[1], 2))
-                self.deque_z.appendleft(np.round(a[2], 2))
+    def lauch_mode_filter(self, sample_size):
+        self.sample_size = sample_size
+        while len(self.deque_x) < self.sample_size:
+            raw_pos = self.mqtt_callback()
+            if raw_pos:
+                self.deque_x.appendleft(np.round(raw_pos[0], 2))
+                self.deque_y.appendleft(np.round(raw_pos[1], 2))
+                self.deque_z.appendleft(np.round(raw_pos[2], 2))
 
                 x_mode = self.my_mode(self.deque_x)
                 y_mode = self.my_mode(self.deque_y)
@@ -73,9 +83,74 @@ class Signal_filter():
                         np.mean(x_mode, axis=0), np.mean(y_mode, axis=0), np.mean(z_mode, axis=0)]
                     return self.final_pos
 
+    def lauch_std_filter(self, sample_size, std_multiplier=1):
+        self.sample_size = sample_size
+        self.unlock = True
+        self.pop_x = None
+        self.pop_y = None
+        self.pop_z = None
+        while len(self.deque_x) < self.sample_size:  # 15 was nice
+            raw_pos = self.mqtt_callback()
+            if raw_pos:
+                while len(self.deque_x) != self.sample_size-1 and self.unlock:
+                    raw_pos_while = self.mqtt_callback()
+                    self.deque_x.appendleft(raw_pos_while[0])
+                    self.deque_y.appendleft(raw_pos_while[1])
+                    self.deque_z.appendleft(raw_pos_while[2])
+                self.unlock = False
+
+                "getting first mean and std"
+                self.mean_xyz = [np.mean(self.deque_x), np.mean(
+                    self.deque_y), np.mean(self.deque_z)]
+
+                self.std_xyz = [np.std(self.deque_x), np.std(
+                    self.deque_y), np.std(self.deque_z)]
+
+                "xyz standard deviation checker"
+                # x
+                if (self.mean_xyz[0]-std_multiplier*self.std_xyz[0]) < (raw_pos[0]) < (self.mean_xyz[0]+std_multiplier*self.std_xyz[0]):
+                    self.deque_x.appendleft(raw_pos[0])
+                else:
+                    # continue
+                    self.deque_x.appendleft(
+                        np.mean(self.deque_x)+self.plus_minus()*np.std(self.deque_x))
+
+                # y
+                if (self.mean_xyz[1]-std_multiplier*self.std_xyz[1]) < (raw_pos[1]) < (self.mean_xyz[1]+std_multiplier*self.std_xyz[1]):
+                    self.deque_y.appendleft(raw_pos[1])
+                else:
+                    self.deque_y.appendleft(
+                        np.mean(self.deque_y)+self.plus_minus()*np.std(self.deque_y))
+
+                # z
+                if (self.mean_xyz[2]-std_multiplier*self.std_xyz[2]) < (raw_pos[2]) < (self.mean_xyz[2]+std_multiplier*self.std_xyz[2]):
+                    self.deque_z.appendleft(raw_pos[2])
+                else:
+                    self.deque_z.appendleft(
+                        np.mean(self.deque_z)+self.plus_minus()*np.std(self.deque_z))
+
+                "popping"
+                if len(self.deque_x) == self.sample_size:
+                    self.pop_x = self.deque_x.pop()
+
+                if len(self.deque_y) == self.sample_size:
+                    self.pop_y = self.deque_y.pop()
+
+                if len(self.deque_z) == self.sample_size:
+                    self.pop_z = self.deque_z.pop()
+
+                "final list"
+                self.final_pos = [
+                    self.pop_x,
+                    self.pop_y,
+                    self.pop_z
+                ]
+                return self.final_pos
+
 
 if __name__ == "__main__":
-    test = Signal_filter(sample_size=15,sigma=5)
+    test = Signal_filter()
     while True:
-        # print(test.lauch_gauss_filter())
-        print(test.lauch_mode_filter())
+        print(test.lauch_std_filter(15, 1))
+        # print(test.lauch_gauss_filter(15, 5))
+        # print(test.lauch_mode_filter(15))
